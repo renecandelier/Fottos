f692208a4b69088ead124be301304fd6156b00b9
Rene Candelier <rene.candelier@turner.com>
2018-12-20 12:43:14 -0500

    Build basic layout for UI.
    - Create new storyboard for explore view.
    - Create new cell for categories.
    - Add temporary image for cell.

---
+++ Fottos/Fottos/View Controllers/Explore/ExploreCollectionViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ExploreCollectionViewController.swift
+//  Fottos
+//
+//  Created by Rene Candelier on 12/19/18.
+//  Copyright © 2018 Novus Mobile. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "CategoryCollectionViewCell"
+
+class ExploreCollectionViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! CategoryCollectionViewCell
+        return cell
+    }
+    
+    // MARK: UICollectionViewDelegate
+
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+
+}
